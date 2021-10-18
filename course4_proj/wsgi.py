5dbf26c760378ac7b098c1e8194205f9ab14c832
Patrick <patricke@codio.com>
2021-10-18 14:08:49 -0400

    Create wsgi.py
    File was missing but is needed

---
+++ course4_proj/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for blango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "course4_proj.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
+
+from configurations.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
